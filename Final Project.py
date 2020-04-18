0677479e4381b8a9e804ad9e4915598f83d57a38
codyclaybourn <60197445+codyclaybourn@users.noreply.github.com>
2020-04-18 13:26:18 -0500

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -1,112 +1,80 @@
-FinalProjectV1.py
-# Name:
-# Date:
+#FinalProjectV1.py
 # Code purpose: Do interesting things of little importance
 # Import external libraries
 import numpy as np
-import time
+import time as t
 import math
 pi = 3.14159265359
-
-
 # Import internal programs
-import L2_vector.py as vector
-import L2_log as log
-import L2_inverse_kinematics.py as invkin
-import L2_kinematics.py as kin
-import L1_lidar as lidar
-import L2_speed_control as spd
+# import L2_vector.py as vector
+# import L2_log as log
+import L2_inverse_kinematics as invkin
+# import L2_kinematics.py as kin
+#import L1_lidar as lidar
+import L2_speed_control as sc
 import csv
 
 #takes path data from external csv file and returns populated array
 #returns [x global, y global, speed setting, circle radius] 
 #A positive radius curves to the left and a negative curves to the right
 #Speed settings are 1 through 5, 5 beaing the fastest.
-def getArray():
-    data=numpy.array([][])
-    my_data = numpy.genfromtxt('PathData.csv', delimiter=',')
-    return my_data
-    
-
-#returns distance of straight line or circle in meters
-def distance(x1, y1, x2, y2, theta):
-    if theta == 0:                                                                      #calculation for a straight line
-
-    if theta == 0:                                                                      #calculation for a straight line
-        length = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)   
-        return length
-
-def distance(x1, y1, x2, y2, theta):
-    if theta == 0:                                                                      #calculation for a straight line
-        dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)   
-        return dist
-        
-    else if theta == (pi)/2                                                           # quarter circle
-        dist = (math.sqrt((y2 - y1)**2 + (x2 - x1)**2)) / 2*math.sin((pi/2)/2)
-        length = (dist*(pi/2)) / quarterArcTime 
-        return length 
-        
-    else if theta == (pi)                                                             # semi circle
-        dist = (math.sqrt((y2 - y1)**2 + (x2 - x1)**2)) / 2*math.sin((pi)/2)
-        length = (dist*(pi)) / semiArcTime 
-        return length 
-    else 
-        
-    
-def time(distance, speed):
-    return distance/speed
-    
-    
-def xdot(distance, time):
-    return distance/time
-
-
-def thetadot(radius, time):
-    
-    
-def time(distance, speed):
-    return distance/speed
-    
-    
-def xdot(distance, time):
-    return distance/time
-
-
-def thetadot(radius, time):
-    
-    
-def pd(xdot, thetadot):
-    
+pathdata = np.genfromtxt('PathData.csv', delimiter=',')
 
 
 
+def distance(stepnum):
+	x1=pathdata[stepnum][0]
+	y1=pathdata[stepnum][1]
+	x2=pathdata[stepnum+1][0]
+	y2=pathdata[stepnum+1][1]
+	theta=pathdata[stepnum][3]
+
+	if theta == 0:                                                                  # calculation for a straight line
+		dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+		length = dist
+		return length
+
+	elif abs(theta) == 1.57:                                                           # quarter circle (might need | -pi/2)
+		dist = (math.sqrt((y2 - y1)**2 + (x2 - x1)**2)) / 2*math.sin((pi/2)/2)
+		length = dist * abs(pi/2)
+		return length 
+
+	elif abs(theta) == 3.14:                                                             # semi circle
+		dist = (math.sqrt((y2 - y1)**2 + (x2 - x1)**2)) / 2*math.sin((pi)/2)
+		length = dist * abs(pi) 
+		return length 
 
 
+def xdotthetadot(stepnum):
+	theta=pathdata[stepnum][3]
+	speed=pathdata[stepnum][2]
+	print(distance(stepnum))
+	time=distance(stepnum)/.4
+	xdot=.4/(6-speed)
+	thetadot=(theta/time)/(6-speed)
+	print(theta,"/",time,"/",speed,"=",thetadot)
+	return np.array([xdot,thetadot])
 
+def time(stepnum):
+	return distance(stepnum)/.4
 
-
-
+def step(stepnum):
+	myPhiDots=invkin.convert(xdotthetadot(stepnum))
+	sc.driveOpenLoop(myPhiDots)
+	
+def drive():
+	for num in range(13):
+		start=t.time()
+		timeDifference=t.time()-start
+		duration=time(num)
+		while timeDifference<duration:
+			timeDifference=t.time()-start
+			print(num)
+			step(num)
+		
+		
+	print("Thats all folks")
+	
 # Start the program
 print("Running FinalProjectV1.py")
-#while(1):
-print(getArray())
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    time.sleep(0.2)
-
-    
-    
+drive()
